0580977146aeed3362849910038f11f3bb5abdb7
mohammad_rizwan <rizwanm76@yahoo.com>
2022-08-15 13:59:45 -0400

    added npm

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,17 +1,17 @@
-import { RECEIEVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session";
+// import { RECEIEVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session";
 
-const _nullSession = {
-  currentUser: null
-}
+// const _nullSession = {
+//   currentUser: null
+// }
 
-export default (state = _nullSession, action) => {
-  Object.freeze(state)
-  switch (action.type) {
-    case RECEIEVE_CURRENT_USER:
-      return Object.assign({}, { currentUser: action.user })
-    case LOGOUT_CURRENT_USER:
-      return _nullSession
-    default:
-      return state
-  }
-}
+// export default (state = _nullSession, action) => {
+//   Object.freeze(state)
+//   switch (action.type) {
+//     case RECEIEVE_CURRENT_USER:
+//       return Object.assign({}, { currentUser: action.user })
+//     case LOGOUT_CURRENT_USER:
+//       return _nullSession
+//     default:
+//       return state
+//   }
+// }
