d677805a7a4cd4f1b33cdb9761399894d87f5be5
mohammad_rizwan <rizwanm76@yahoo.com>
2022-08-19 13:50:21 -0400

    added logos to navbar

--- frontend/components/feed/header.jsx
+++ frontend/components/feed/header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 class Header extends React.Component {
     render() {
@@ -8,10 +10,19 @@ class Header extends React.Component {
 
 
             <div className="header-left">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
+            <Link to="/feed">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" className="logo" />
+            </Link>
+            <input type="text" placeholder="Search FaceDiary" />
             </div>
 
-            <div className="header-middle"></div>
+            <div className="header-middle">
+                <Link to="/feed">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2mzkC_s6MVaJhbJG-krLC16KQuVwBCu0cWYg9AcJJJ4uSQnjbrEz9hjpNGCCuMVwzuAU&usqp=CAU" alt="" className="logo" />
+                </Link>
+                <a href="https://github.com/mrizwan83/FaceDiary"><img src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="" className="logo"/></a>
+                <a href="https://linkedin.com/in/mohammad-rizwan-a83a31246"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="" className="logo" /></a>
+            </div>
 
             <div className="header-right"></div>
             
