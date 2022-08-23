bd04b30c8c3974e5732abee66eeee48c887057cf
mohammad_rizwan <rizwanm76@yahoo.com>
2022-08-23 12:34:58 -0400

    added profile-pic place holder and name

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -12,17 +12,29 @@ class Profile extends React.Component {
             <div>
                 <HeaderContainer />
                 <div className='profile-page'>
+                    <div id="profile-header">
                     <div className='profile-header'>
+
+                    {/* place holder for photo */}
                     <div className='cover-photo-container'>
+
+                        {/* this div should be replaced with the cover photo image */}
                         <div className='cover-photo'>
+
+                            {/* button */}
                             <div className='update-cover-photo'>
                                 <img src="https://toppng.com/uploads/preview/appareil-photo-icon-camera-icon-small-11553511694u4myfjqg7j.png" alt="" id="camera-logo" />
                                 Update Cover Photo
                             </div>
                         </div>
+                    </div>
 
-                        <div className='profile-photo'>
-                            
+                        {/* profile pic container */}
+                        <div id="bottom-profile-header">
+                        <div className='profile-photo-container'>
+                        <div className="update-profile-photo"><img src="https://toppng.com/uploads/preview/appareil-photo-icon-camera-icon-small-11553511694u4myfjqg7j.png" alt="" id="camera-logo-2" /></div>
+                        </div>
+                        <div className='profile-username'>Demo User</div>
                         </div>
                     </div>
                     </div>
