5384e8d7cadd69641b0b1d2cc6b6890f6503b32c
mohammad_rizwan <rizwanm76@yahoo.com>
2022-08-23 17:33:53 -0400

    fixed bug with refreshing and loging out, it works now

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -40,16 +40,30 @@ class Profile extends React.Component {
                         
                     </div>
                     </div>
+                    </div>
                     <div className="profile-body">
                         <div className="profile-bio-section">
-                                This is where profile bio section goes
+                            <div className="profile-bio">Bio</div>
                         </div>
 
                         <div className="profile-post-container">
-                                this will house the right side for posts
+                           
+                            <div className="profile-middle-post">
+                                <div className="middle-post-top">
+                                    <img src="https://powerusers.microsoft.com/t5/image/serverpage/image-id/98171iCC9A58CAF1C9B5B9/image-size/large/is-moderation-mode/true?v=v2&px=999" alt="" id="post-pic-logo" />
+                                    <input type="text" placeholder="What's on your mind, User?" id="post-input-feed"/>
+                                </div>
+
+                            <div className="middle-post-bottom">
+                   
+                            <div className="middle-post-item"><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/11204-200.png" alt="" className="post-photo-logo" /><div id="photo-button-feed">Photo</div></div>
+                            </div>
+                        </div>
+
+                            <div>why isnt this working</div>
                         </div>
                     </div>
-                </div>
+            
             </div>
         )
     }
