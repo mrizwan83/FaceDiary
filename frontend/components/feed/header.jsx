b0765ca8ad58c8e0dea3369beef4221cd9e57ba6
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-05 18:43:40 -0400

    add angel list link

--- frontend/components/feed/header.jsx
+++ frontend/components/feed/header.jsx
@@ -20,6 +20,7 @@ class Header extends React.Component {
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2mzkC_s6MVaJhbJG-krLC16KQuVwBCu0cWYg9AcJJJ4uSQnjbrEz9hjpNGCCuMVwzuAU&usqp=CAU" alt="" className="logo" />
                 </Link>
                 <a href="https://github.com/mrizwan83/FaceDiary" target="_blank"><img src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="" className="logo"/></a>
+                <a href="https://angel.co/u/mohammad-h-rizwan" target="_blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhGOMqJ4VmhTJxuQ3ejv4jyki4ZfZQJURn7F8uGmBALA&s" alt="" className="logo"/></a>
                 <a href="https://linkedin.com/in/mohammad-rizwan-a83a31246" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="" className="logo" /></a>
             </div>
 
