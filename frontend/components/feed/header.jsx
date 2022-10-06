9d05b5b791b6c64de6b767bb11fdaa511cf23ca5
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-06 16:49:42 -0400

    fix linked in link

--- frontend/components/feed/header.jsx
+++ frontend/components/feed/header.jsx
@@ -21,7 +21,7 @@ class Header extends React.Component {
                 </Link>
                 <a href="https://github.com/mrizwan83/FaceDiary" target="_blank"><img src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png" alt="" className="logo"/></a>
                 <a href="https://angel.co/u/mohammad-h-rizwan" target="_blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhGOMqJ4VmhTJxuQ3ejv4jyki4ZfZQJURn7F8uGmBALA&s" alt="" className="logo"/></a>
-                <a href="https://linkedin.com/in/mohammad-rizwan-a83a31246" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="" className="logo" /></a>
+                <a href="https://www.linkedin.com/in/mohammad-h-rizwan-a83a31246/" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png" alt="" className="logo" /></a>
             </div>
 
             <div className="header-right">
