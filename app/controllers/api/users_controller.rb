82cf01eeee24671ac8bf0f3baaa1b20b1f579946
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-26 22:35:55 -0400

    add friend request form demo user when creating account

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,6 +4,9 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             login(@user)
+            @demo = User.find_by(email: 'demo@appacademy.io')
+            @friend = Friend.new(requester_id: @demo.id, requestee_id: @user.id)
+            @friend.save
             render :show
         else
             render json: @user.errors.full_messages, status: 422
